43caf9b27f88cd767d37e46477b2c8d3e5798247
zhaohua <zhengzhsam@gmail.com>
2022-10-15 18:50:44 -0500

    structure

--- daily_marine_creature/android/app/src/main/java/com/daily_marine_creature/MainActivity.java
+++ daily_marine_creature/android/app/src/main/java/com/daily_marine_creature/MainActivity.java
@@ -3,7 +3,7 @@ package com.daily_marine_creature;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-
+import android.os.Bundle;
 public class MainActivity extends ReactActivity {
 
   /**
@@ -14,6 +14,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "daily_marine_creature";
   }
+  @Override
+protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
---
+++ daily_marine_creature/navigation/index.tsx
@@ -0,0 +1,76 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
+import * as React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import IonIcons from 'react-native-vector-icons/Ionicons';
+import { ColorSchemeName, Pressable, TabBarIOSItem, Text } from 'react-native';
+import HomeScreen from "../screens/homepage/HomepageScreen";
+import DailyCreatureScreen from "../screens/daily_creature_page/DailyCreatureScreen";
+import OpportunityScreen from "../screens/opportunity_page/OpportunityScreen";
+
+export type RootStackParams = {
+    HomeStack: undefined;
+    DailyCreatureStack: undefined;
+    OpportunityStack: undefined;
+}
+const Tab = createBottomTabNavigator<RootStackParams>();
+
+export default function Navigation() {
+    return (
+        <NavigationContainer >
+            <Tab.Navigator screenOptions={
+                ({route}) => ({
+                    tabBarIcon: ({focused, color, size}) => {
+                        let iconName;
+                        if (route.name == 'HomeStack') {
+                            iconName = focused ? 'ios-home' : 'ios-home-outline'
+                            return <IonIcons name={ iconName } size={ size } color={color} />
+                        } else if (route.name == 'DailyCreatureStack') {
+                            iconName = focused ? 'file-tray-stacked-sharp': 'file-tray-stacked-outline'
+                            return <IonIcons name={ iconName } size={ size } color={color} />
+                        } else if (route.name == 'OpportunityStack') {
+                            iconName = focused ? 'ios-newspaper' : 'ios-newspaper-outline'
+                            return <IonIcons name={ iconName } size={ size } color={color} />
+                    }
+                    },
+                    headerShown: false
+                })
+            }>
+                <Tab.Screen name="HomeStack" component={HomeStackScreen} options={{tabBarLabel: "homepage", headerTransparent: true}}/>
+                <Tab.Screen name="DailyCreatureStack" component={DailyCreatureStackScreen} options={{tabBarLabel: "Daily Creature", headerTransparent: true}}/>
+                <Tab.Screen name="OpportunityStack" component={OpportunityStackScreen} options={{tabBarLabel: "Opportunities", headerTransparent: true}}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
+
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="Home" component={HomeScreen}/>
+        </HomeStack.Navigator>
+    )
+}
+
+const DailyCreatureStack = createNativeStackNavigator();
+
+function DailyCreatureStackScreen() {
+    return (
+        <DailyCreatureStack.Navigator screenOptions={{ headerTransparent: false }}>
+            <DailyCreatureStack.Screen name="daily_creature" component={DailyCreatureScreen} />
+        </DailyCreatureStack.Navigator>
+    )
+}
+
+const OpportunityStack = createNativeStackNavigator();
+
+function OpportunityStackScreen() {
+    return (
+        <OpportunityStack.Navigator>
+            <OpportunityStack.Screen name="opportunity" component={OpportunityScreen}/>
+        </OpportunityStack.Navigator>
+    )
+}
